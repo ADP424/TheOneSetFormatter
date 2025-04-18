2009e25bd0bbf08ab335813136a20b936d534eaa
ADP424 <abdalgarnoplatt@gmail.com>
2025-04-18 14:34:14 -0400

    Added poker

--- src/log.py
+++ src/log.py
@@ -1,9 +1,10 @@
 def reset_log():
-    with open("log.txt", 'w', encoding="utf8") as log_file:
+    with open("log.txt", "w", encoding="utf8") as log_file:
         log_file.write("")
 
+
 def log(message: str, do_print=True):
-    with open("log.txt", 'a', encoding="utf8") as log_file:
+    with open("log.txt", "a", encoding="utf8") as log_file:
         log_file.write(message)
     if do_print:
         print(message)
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import csv
+import argparse
 from datetime import datetime
 from PIL import Image
 
@@ -14,9 +15,12 @@ TRANSFORM_BACKSIDES = "spreadsheets/transform.csv"
 CARD_NAME = "Card Name"
 FRONT_CARD_NAME = "Front Card Name"
 CARD_RARITY = "Rarity"
+CARD_COLOR = "Color Identity"
 CARD_TYPES = "Type(s)"
 CARD_SUBTYPES = "Subtype(s)"
+CARD_SUPERTYPES = "Supertype(s)"
 CARD_DATE = "Date Created"
+ARCHETYPE = "Archetype"
 
 
 CHAR_TO_TITLE_CHAR = {
@@ -44,8 +48,14 @@ NUMBER_WIDTHS = {
     "9": 25,
 }
 
+COLORS = {"W": "White", "U": "Blue", "B": "Black", "R": "Red", "G": "Green"}
 
-def process_spreadsheets() -> dict[str, dict[str, str | dict[str, str]]]:
+POKER_BORDERS = {"W": "fold", "U": "echo", "B": "necro", "R": "joker", "G": "wild", "Colorless": "glass"}
+
+
+def process_spreadsheets() -> (
+    tuple[dict[str, dict[str, str | dict[str, str]]], dict[str, dict[str, str]]]
+):
     cards = {}
     with open(CARDS, "r", encoding="utf8") as cards_sheet:
         cards_sheet_reader = csv.reader(cards_sheet)
@@ -64,7 +74,16 @@ def process_spreadsheets() -> dict[str, dict[str, str | dict[str, str]]]:
             if len(values[CARD_NAME]) > 0:
                 cards[values[FRONT_CARD_NAME]]["Transform Backsides"].append(values)
 
-    return cards
+    tokens = {}
+    with open(TOKENS, "r", encoding="utf8") as tokens_sheet:
+        tokens_sheet_reader = csv.reader(tokens_sheet)
+        columns = next(tokens_sheet_reader)
+        for row in tokens_sheet_reader:
+            values = dict(zip(columns, row))
+            if len(values[CARD_NAME]) > 0:
+                tokens[values[CARD_NAME]] = values
+
+    return cards, tokens
 
 
 def cardname_to_filename(card_name: str) -> str:
@@ -94,11 +113,15 @@ def open_card_file(file_name: str) -> Image.Image | None:
     return base_card
 
 
-def main():
-    cards = process_spreadsheets()
+def process_cards(cards: dict[str, dict[str, str | dict[str, str]]]):
+    log("----- PROCESSING CARDS -----\n")
+
     card_name_list = list(cards.keys())
     card_name_list.sort(
-        key=lambda card_name: datetime.strptime(cards[card_name][CARD_DATE], "%m/%d/%Y")
+        key=lambda card_name: (
+            datetime.strptime(cards[card_name][CARD_DATE], "%m/%d/%Y"),
+            cards[card_name][CARD_NAME],
+        )
     )
 
     for num, card_name in enumerate(card_name_list):
@@ -125,8 +148,13 @@ def main():
 
         card_overlay = Card(card_width, card_height)
 
-        # TODO: Decide a way to determine if card has special poker border
-        card_overlay.add_layer(f"images/{frame_type}/borders/black.png")
+        archetype = card[ARCHETYPE]
+        if "Poker" in archetype:
+            color = card[CARD_COLOR].strip()
+            border = POKER_BORDERS.get(color, "black")
+            card_overlay.add_layer(f"images/{frame_type}/borders/{border}.png")
+        else:
+            card_overlay.add_layer(f"images/{frame_type}/borders/black.png")
 
         card_overlay.add_layer(f"images/{frame_type}/collection/set_name.png")
 
@@ -151,6 +179,13 @@ def main():
                 )
             offset += NUMBER_WIDTHS[char]
 
+        card_overlay.add_layer(base_card, 0)
+
+        final_card = card_overlay.merge_layers()
+        final_card.save(f"cards/processed_cards/{file_name}.png")
+        log(f"""Successfully processed "{card_name}".""")
+
+        card_overlay.remove_layer(0)
         for backside in card["Transform Backsides"]:
             tf_card_name = backside[CARD_NAME]
             tf_file_name = cardname_to_filename(tf_card_name)
@@ -164,12 +199,96 @@ def main():
             final_card.save(f"cards/processed_cards/{tf_file_name}.png")
             log(f"""\tSuccessfully processed "{tf_card_name}".""")
 
-        card_overlay.add_layer(base_card, 0)
 
-        final_card = card_overlay.merge_layers()
-        final_card.save(f"cards/processed_cards/{file_name}.png")
-        log(f"""Successfully processed "{card_name}".""")
+def process_tokens(tokens: dict[str, dict[str, str]]):
+    log("----- PROCESSING TOKENS -----\n")
+
+    token_name_list = list(tokens.keys())
+    token_name_list.sort(
+        key=lambda token_name: (
+            datetime.strptime(tokens[token_name][CARD_DATE], "%m/%d/%Y"),
+            tokens[token_name][CARD_NAME],
+        )
+    )
+
+    for num, token_name in enumerate(token_name_list):
+
+        token = tokens[token_name]
+
+        color = token[CARD_COLOR]
+        if "Colorless" in color:
+            color = "Colorless "
+        else:
+            color = ""
+            for char in color.strip():
+                try:
+                    color += f"{COLORS[char]} "
+                except:
+                    log(f'"{token_name}" has an invalid color identity.')
+                    color = ""
+                    break
+
+        if len(color) == 0:
+            continue
+
+        token_supertypes = tokens[token_name][CARD_SUPERTYPES]
+        token_types = tokens[token_name][CARD_TYPES]
+        full_token_name = f"{color}{token_supertypes} {token_name} {token_types}"
+
+        file_name = cardname_to_filename(full_token_name)
+
+        base_token = open_card_file(file_name)
+        if base_token is None:
+            continue
+
+        token_overlay = Card()
+
+        token_overlay.add_layer("images/standard/borders/black.png")
+
+        token_overlay.add_layer("images/standard/collection/set_name.png")
+
+        year = datetime.strptime(token[CARD_DATE], "%m/%d/%Y").year
+        token_overlay.add_layer(f"images/standard/years/{year}.png")
+
+        number = str(num + 1).zfill(len(str(len(tokens))))
+        offset = 0
+        for char in number:
+            token_overlay.add_layer(
+                f"images/standard/numbers/{char}.png",
+                position=(int(offset), 0),
+            )
+            offset += NUMBER_WIDTHS[char]
+
+        token_overlay.add_layer(base_token, 0)
+
+        final_token = token_overlay.merge_layers()
+        final_token.save(f"cards/processed_cards/{file_name}.png")
+        log(f"""Successfully processed "{file_name}".""")
+
+
+def main(do_cards: bool = True, do_tokens: bool = True):
+    cards, tokens = process_spreadsheets()
+    if do_cards:
+        process_cards(cards)
+    if do_tokens:
+        process_tokens(tokens)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Format The One Set cards.")
+
+    parser.add_argument(
+        "-nc", "--no-cards",
+        action="store_false",
+        help="Skip processing the regular cards.",
+        dest="cards"
+    )
+    parser.add_argument(
+        "-nt", "--no-tokens",
+        action="store_false",
+        help="Skip processing the tokens.",
+        dest="tokens"
+    )
+
+    args = parser.parse_args()
+    main(args.cards, args.tokens)
